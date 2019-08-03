4fbc7422fce928daef8f1472273241975923e3eb
joeandrewkey <joeandrewkey@gmail.com>
2019-08-03 11:18:58 -0700

    Adding a timer and some console text coloring.

--- problem_picker.py
+++ problem_picker.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import os
+import time
+import sys
+from termcolor import colored, cprint
 
 
 def get_questions_df(file_name):
@@ -19,9 +22,20 @@ def get_all_questions(directory_path):
 
 
 if __name__ == "__main__":
-    print("Set up a simple CLI to offer hints, and throw up a timer.")
+    print("Set up a simple CLI to pick a random question, and throw up a timer.")
     df = get_all_questions("./data")
     new_df = df.sample(n=1)
     q = list(new_df["Question"])[0]
     t = list(new_df["Title"])[0]
-    print(f"Title:\n\t{t}\nQuestion:\n\t{q}")
+    cprint(f"Title:", "green")
+    print(f"\t{t}")
+    cprint(f"Question:", "yellow")
+    print(f"\t{q}")
+
+    time_for_problem = 10
+    cprint("Time Remaining", 'red')
+    for remaining in range(time_for_problem, 0, -1):
+        sys.stdout.write("\r")
+        sys.stdout.write(f"\t{remaining:4d} seconds.")
+        sys.stdout.flush()
+        time.sleep(1)
