b5a17622e7fd25cf7b7968514e6807f5fcb12d64
joeandrewkey <joeandrewkey@gmail.com>
2019-08-03 18:26:03 -0700

    Adding chapter 7.

--- problem_picker.py
+++ problem_picker.py
@@ -27,20 +27,15 @@ def get_all_questions(directory_path):
     return df
 
 
-if __name__ == "__main__":
-    """This script picks a random question and presents it to the user to solve in a limited amout of time"""
-    # Parse Arguments
+def parse_arguments():
+    """Parses all command line arguments"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("time_for_problem", help="Number of seconds for the problem", type=int)
-    args = parser.parse_args()
+    parser.add_argument("time_for_problem", help="Integer umber of seconds for the problem", type=int)
+    return parser.parse_args()
 
-    # Collect questions from data directory
-    df = get_all_questions("./data")
 
-    # Prompt user to start
-    input(f"You will have {args.time_for_problem} seconds to answer the question, press ENTER to start")
-
-    # Display a random question
+def display_random_question(df):
+    """Display a random question selected from the supplied dataframe"""
     new_df = df.sample(n=1)
     question = list(new_df["Question"])[0]
     title = list(new_df["Title"])[0]
@@ -48,8 +43,11 @@ if __name__ == "__main__":
     print(f"{title}\n")
     cprint("Question:", "yellow")
     print(f"{question}\n")
+    return new_df
 
-    # Show a timer
+
+def display_timer():
+    """Display a timer, that can be interrupted with CTRL + C"""
     cprint("You can press CTRL + C at any time to stop the timer", "blue")
     cprint("Time Remaining", 'red')
     keep_running = True
@@ -68,13 +66,26 @@ if __name__ == "__main__":
         cprint(f"Processing keyboard interrupt.", "blue")
 
     if not keep_running:
-        print(f"Time's up!")
+        print(f"You ran out of time!")
     else:
-        print(f"There were {args.time_for_problem - counter} seconds remaining.")
+        print(f"You finished with {args.time_for_problem - counter} seconds remaining.")
 
-    major = list(new_df["Major"])[0]
-    minor = list(new_df["Minor"])[0]
-    print(f"\nThe program selected Chapter {major} Question {minor}")
+
+if __name__ == "__main__":
+    """This script picks a random question and presents it to the user to solve in a limited amout of time"""
+    args = parse_arguments()
+    # Collect questions from data directory
+    df = get_all_questions("./data")
+
+    # Prompt user to start
+    input(f"You will have {args.time_for_problem} seconds to answer the question, press ENTER to start")
+
+    new_df = display_random_question(df)
+    display_timer()
+
+    chapter = list(new_df["Major"])[0]
+    question = list(new_df["Minor"])[0]
+    print(f"\nThe program selected Chapter {chapter} Question {question}")
 
 
 
