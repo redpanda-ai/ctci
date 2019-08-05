84ec182b1fe586b67ad75b19916fe468bef068ea
joeandrewkey <joeandrewkey@gmail.com>
2019-08-05 16:36:45 -0700

    Fixing eight queens.

--- eight_queens.py
+++ eight_queens.py
@@ -1,99 +1,71 @@
-import sys
-board = [[0 for i in range(8)] for j in range(8)]
 
-def print_board():
-    for i in range(8):
+def get_clear_board():
+    return [[0 for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
+
+
+def print_board(board):
+    for i in range(BOARD_SIZE):
         print(board[i])
 
 
-def safe(i, j):
-    global board
-    #print("safety check")
-    v = sum([board[x][j] for x in range(8) if x != i])
+def safe(board, i, j):
+    v = sum([board[x][j] for x in range(BOARD_SIZE) if x != i])
     if v != 0:
         return False
-    h = sum([board[i][y] for y in range(8) if y != j])
+    h = sum([board[i][y] for y in range(BOARD_SIZE) if y != j])
     if h != 0:
         return False
 
-    d1 = []
-    for a in range(1, 8):
-        if (i + a) < 8 and (j + a) < 8:
-            d1.append(board[i+a][j+a])
-            # d1.append(((i + a), (j + a)))
+    d = []
+    for a in range(1, BOARD_SIZE):
+        if (i + a) < BOARD_SIZE and (j + a) < BOARD_SIZE:
+            d.append(board[i + a][j + a])
         if (i - a) >= 0 and (j - a) >= 0:
-            d1.append(board[i-a][j-a])
-            # d1.append(((i - a), (j - a)))
-        if (i + a) < 8 and (j - a) >= 0:
-            d1.append(board[i + a][j - a])
-            # d1.append(((i + a), (j - a)))
-        if (i - a) >= 0 and (j + a) < 8:
-            d1.append(board[i-a][j+a])
-            # d1.append(((i - a), (j + a)))
+            d.append(board[i - a][j - a])
+        if (i + a) < BOARD_SIZE and (j - a) >= 0:
+            d.append(board[i + a][j - a])
+        if (i - a) >= 0 and (j + a) < BOARD_SIZE:
+            d.append(board[i - a][j + a])
 
-    d1 = sum(d1)
 
-    if d1 != 0:
+    d2 = []
+    signs = [(1, 1), (-1, -1), (1, -1), (-1, 1)]
+    for sign in signs:
+        w = [board[i + sign[0] * b][j + sign[1] * b]
+             for b in range(1, BOARD_SIZE)
+             if 0 <= i + sign[0] * b < BOARD_SIZE and 0 <= j + sign[1] * b < BOARD_SIZE]
+        d2.extend(w)
+
+    d = sum(d2)
+    if d != 0:
         return False
 
     return True
 
 
-def mark(queen_num, i, j):
-    global board
-    for x in range(8):
-        if board[x][j] == 0:
-            board[x][j] = queen_num
+def solve(board=None, queen_num=1, start_a=0):
+    if not board:
+        board = get_clear_board()
 
-    for y in range(8):
-        if board[i][y] == 0:
-            board[i][y] = queen_num
-
-    d1 = []
-    for a in range(1, 8):
-        if (i + a) < 8 and (j + a) < 8:
-            if safe(i+a, j+a):
-                board[i+a][j+a] = queen_num
-        if (i - a) >= 0 and (j - a) >= 0:
-            if safe(i-a, j-a):
-                board[i-a][j-a] = queen_num
-        if (i + a) < 8 and (j - a) >= 0:
-            if safe(i+a, j-a):
-                board[i+a][j-a] = queen_num
-        if (i - a) >= 0 and (j + a) < 8:
-            if safe(i-a, j+a):
-                board[i-a][j+a] = queen_num
-
-def safe_new(i, j):
-    if board[i][j] == 0:
-        return True
-    return False
-
-
-def solve(queen_num):
-    global board
-    #print_board()
-    #print(f"QN: {queen_num}")
-    if queen_num == 9:
+    if queen_num == NUM_QUEENS + 1:
         print("Finished")
-        print_board()
-        sys.exit()
-    for i in range(8):
-        for j in range(8):
-            print(f"q: {queen_num}, i: {i}, j: {j}")
-            if board[i][j] == 0:
-                if safe(i, j):
-                    #print(f"Placing {queen_num} at {i}, {j}")
-                    board[i][j] = queen_num
-                    #mark(queen_num, i, j)
-                    print_board()
-                    solved = solve(queen_num + 1)
-                    if not solved:
-                        board[i][j] = 0
-                #else:
-                    # print(f"Cannot Place {queen_num} at {i}, {j}")
+        print_board(board)
+        return True
+    for a in range(start_a, BOARD_SIZE ** 2):
+        i = a // BOARD_SIZE
+        j = a % BOARD_SIZE
+
+        if safe(board, i, j):
+            board[i][j] = queen_num
+            solved = solve(board=board, queen_num=(queen_num + 1), start_a=(a + 1))
+            if not solved:
+                board[i][j] = 0
+            else:
+                return solved
 
     return False
 
 
-solve(1)
+NUM_QUEENS = 8
+BOARD_SIZE = 8
+solve()
