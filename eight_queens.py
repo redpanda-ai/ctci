816753feba41e88d4ab18b9964fc291be75dca7c
joeandrewkey <joeandrewkey@gmail.com>
2019-08-05 18:28:56 -0700

    Finishing eight_queens

--- eight_queens.py
+++ eight_queens.py
@@ -12,7 +12,6 @@ def safe_on_diagonals(board, i, j):
     global safe_count
     safe_count += 1
 
-    #check diagonals
     for a in range(1, BOARD_SIZE):
         if (i + a) < BOARD_SIZE and (j + a) < BOARD_SIZE:
             if board[i + a][j + a] != 0:
@@ -30,29 +29,35 @@ def safe_on_diagonals(board, i, j):
     return True
 
 
-def solve(board=None, queen_num=1, start_cell=0, cols=None):
-    if not board:
-        board = get_clear_board()
-    if not cols:
-        cols = set()
+def solve(board=None, queen_num=1, start_cell=0, used_cols=None):
+
+    # Success case
     if queen_num == NUM_QUEENS + 1:
         print_board(board)
         return True
 
+    # create an empty chess board
+    if not board:
+        board = get_clear_board()
+
+    # create a set to track used columns
+    if not used_cols:
+        used_cols = set()
+
     # don't inspect cells where the column has already been used
-    cells = [i + start_cell for i in range(BOARD_SIZE) if i not in cols]
+    cells = [i + start_cell for i in range(BOARD_SIZE) if i not in used_cols]
 
     for cell in cells:
         i = cell // BOARD_SIZE
         j = cell % BOARD_SIZE
         if safe_on_diagonals(board, i, j):
             board[i][j] = queen_num
-            cols.add(j)
-            start_cell = (i + 1) * BOARD_SIZE  # start at next row, it saves time
-            solved = solve(board=board, queen_num=(queen_num + 1), start_cell=start_cell, cols=cols)
+            used_cols.add(j)
+            start_cell = (i + 1) * BOARD_SIZE  # skip to the next row
+            solved = solve(board=board, queen_num=(queen_num + 1), start_cell=start_cell, used_cols=used_cols)
             if not solved:
                 board[i][j] = 0
-                cols.remove(j)
+                used_cols.remove(j)
             else:
                 return solved
 
@@ -60,7 +65,7 @@ def solve(board=None, queen_num=1, start_cell=0, cols=None):
 
 
 if __name__ == "__main__":
-    for test in list(range(1, 20)):
+    for test in list(range(1, 21)):
         print(f"{test} x {test}")
         safe_count = 0
         NUM_QUEENS = test
