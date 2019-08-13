fb07d02604da76338b15c90036a1f776ea97952a
joeandrewkey <joeandrewkey@gmail.com>
2019-08-13 12:23:01 -0700

    quick_sort

---
+++ solutions/quick_sort.py
@@ -0,0 +1,27 @@
+def partition(arr, low, high):
+    i = low - 1
+    pivot = arr[high]
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quick_sort(arr, low, pi -1)
+        quick_sort(arr, pi + 1, high)
+
+
+if __name__ == "__main__":
+    test = [10, 7, 8, 9, 1, 5]
+    n = len(test)
+    quick_sort(test, 0, n-1)
+    print(test)
+
+
